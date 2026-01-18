0aae37cc209546a3aa62da858995befbaa117068
LucasErrNotFound <robertxlucas21@protonmail.com>
2026-01-18 21:21:46 +0800

    feat: add configuration module for bot settings and constants

---
+++ MyBot/include/Config.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <string>
+
+namespace BotConfig {
+	extern const std::string BOT_TOKEN;
+	extern const std::string COBALT_API;
+	extern const std::string DOWNLOAD_DIR;
+	extern const bool KEEP_DOWNLOADED_FILES;
+}
