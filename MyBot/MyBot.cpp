6287f85fd27d31d9634a890234decacba7757c6b
LucasErrNotFound <robertxlucas21@protonmail.com>
2026-01-18 21:54:58 +0800

    fix: configure main file (I forgot)

--- MyBot/MyBot.cpp
+++ MyBot/MyBot.cpp
@@ -1,45 +1,23 @@
-#include "MyBot.h"
+#include "include/Config.h"
+#include "include/BotCommands.h"
 #include <dpp/dpp.h>
 
-/* Be sure to place your token in the line below.
- * Follow steps here to get a token:
- * https://dpp.dev/creating-a-bot-application.html
- * When you invite the bot, be sure to invite it with the 
- * scopes 'bot' and 'applications.commands', e.g.
- * https://discord.com/oauth2/authorize?client_id=940762342495518720&scope=bot+applications.commands&permissions=139586816064
- */
-const std::string    BOT_TOKEN    = "add your token here";
-
-int main()
-{
-	/* Create bot cluster */
-	dpp::cluster bot(BOT_TOKEN);
-
-	/* Output simple log messages to stdout */
+int main() {
+	dpp::cluster bot(BotConfig::BOT_TOKEN);
 	bot.on_log(dpp::utility::cout_logger());
 
-	/* Register slash command here in on_ready */
+	bot.on_slashcommand([&bot](const dpp::slashcommand_t& event) {
+		if (event.command.get_command_name() == "inject") {
+			BotCommands::handleInjectCommand(event, bot);
+		}
+		});
+
 	bot.on_ready([&bot](const dpp::ready_t& event) {
-		/* Wrap command registration in run_once to make sure it doesnt run on every full reconnection */
 		if (dpp::run_once<struct register_bot_commands>()) {
-			std::vector<dpp::slashcommand> commands {
-				{ "ping", "Ping pong!", bot.me.id }
-			};
-
-			bot.global_bulk_command_create(commands);
+			BotCommands::registerCommands(bot);
 		}
-	});
+		});
 
-	/* Handle slash command with the most recent addition to D++ features, coroutines! */
-	bot.on_slashcommand([](const dpp::slashcommand_t& event) -> dpp::task<void> {
-		if (event.command.get_command_name() == "ping") {
-			co_await event.co_reply("Pong!");
-		}
-		co_return;
-	});
-
-	/* Start the bot */
 	bot.start(dpp::st_wait);
-
 	return 0;
 }
