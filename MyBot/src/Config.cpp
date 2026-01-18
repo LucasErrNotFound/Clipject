22553ef76c4b7264ba45417f0813af29ee890a99
LucasErrNotFound <robertxlucas21@protonmail.com>
2026-01-18 21:22:23 +0800

    chore: add config for bots

---
+++ MyBot/src/Config.cpp
@@ -0,0 +1,9 @@
+#include <../include/Config.h>
+#include <string>
+
+namespace BotConfig {
+	const std::string BOT_TOKEN = "[DISCORD-BOT-TOKEN]";
+	const std::string COBALT_API = "http://localhost:9000/";
+	const std::string DOWNLOAD_DIR = "./downloads/";
+	const bool KEEP_DOWNLOADED_FILES = false;
+}
